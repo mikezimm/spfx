f53f8acf80ad46df5e098f0f588060bb2fed7ef9
mikezimm <49648086+mikezimm@users.noreply.github.com>
2021-02-19 22:24:46 -0500

    Have animated relaxing data set example

--- FoamTree/gulpfile.js
+++ FoamTree/gulpfile.js
@@ -4,4 +4,7 @@ const build = require('@microsoft/sp-build-web');
 
 build.addSuppression(`Warning - [sass] The local CSS class 'ms-Grid' is not camelCase and will not be type-safe.`);
 
+//Added for this project:  https://stackoverflow.com/questions/50968594/gulp-bundle-ship-the-build-failed-because-a-task-wrote-output-to-stderr
+build.addSuppression(`Warning - tslint - src/webparts/foamtree/FoamtreeWebPart.ts(24,5): error no-unused-expression: unused expression, expected an assignment or function call`);
+
 build.initialize(require('gulp'));
--- FoamTree/src/webparts/foamtree/FoamtreeWebPart.ts
+++ FoamTree/src/webparts/foamtree/FoamtreeWebPart.ts
@@ -15,22 +15,68 @@ export interface IFoamtreeWebPartProps {
 }
 
 export default class FoamtreeWebPart extends BaseClientSideWebPart<IFoamtreeWebPartProps> {
+
   public render(): void {
     this.domElement.innerHTML = `
       <div id="visualization" style="width: 800px; height: 600px">
       </div>`;
 
-    new FoamTree({
+    let foamtree = new FoamTree({
       id: "visualization",
       dataObject: {
         groups: [
+          /*
           { label: "Your", weight: 1.0 },
           { label: "First", weight: 3.0 },
           { label: "FoamTree", weight: 2.0 },
           { label: "Visualization", weight: 4.0 },
+          { label: "BM", weight: 8.0 },
+          { label: "BK", weight: 2.0 },
+          { label: "TTP", weight: 3.5 },
+          */
+          { label: "Bad news", open: true, groups: [
+            { label: "Last penny lost", sentiment: -0.5 },
+            { label: "Bazinga doomed",  sentiment: -1 }
+            ]},
+          { label: "Good news", exposed: true, selected: true, groups: [
+            { label: "iPads under $100",      sentiment: 0.5, selected: true },
+            { label: "Martians are friendly", sentiment: 1 }
+            ]},
+          { label: "Other news", groups: [
+            { label: "Vampires on the loose",     sentiment: -2 },
+            { label: "Van Helsing to the rescue", sentiment: -3 }
+            ]}
         ],
       },
     });
+
+    foamtree.set({
+      fadeDuration: 1500,
+      dataObject: {
+        groups: (function() {
+          var arr = [];
+          for (var i = 0; i < 80; i++) {
+            arr.push({
+              label: "a",
+              weight: Math.pow(Math.random(), 5) +
+                      (Math.random() < 0.1 ? Math.random() * 2 : 0)
+            });
+          }
+          return arr;
+        })()
+      },
+    
+      // Show the relaxation
+      relaxationVisible: true,
+    
+      // Make the relaxation last longer
+      relaxationQualityThreshold: 0,
+      relaxationMaxDuration: 15000,
+    
+      // For faster rendering
+      groupFillType: "plain"
+    });
+    
   }
 
   protected getPropertyPaneConfiguration(): IPropertyPaneConfiguration {
